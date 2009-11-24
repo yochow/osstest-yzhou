f04bc51526c3d47d47557965203b6edb6b5bf3f8
Ian Jackson <iwj@woking.uk.xensource.com>
2009-11-24 17:51:07 +0000

    WIP before full install test

--- Osstest.pm
+++ Osstest.pm
@@ -4,6 +4,8 @@ package Osstest;
 use strict;
 use warnings;
 
+use Fcntl;
+
 BEGIN {
     use Exporter ();
     our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
@@ -13,8 +15,10 @@ BEGIN {
                       $tftptail
                       %c $dbh_state
                       readconfig opendb_state selecthost
-                      poll_loop log
+                      poll_loop logm link_file_contents create_webfile
                       power_state
+                      setup_pxeboot setup_pxeboot_local
+                      await_webspace_fetch_byleaf await_sshd
                       );
     %EXPORT_TAGS = ( );
 
@@ -41,6 +45,18 @@ sub opendb ($) {
     return $dbh;
 }
 
+sub target_ssh_root ($$) {
+    my ($ho, $timeout, $tcmd) = @_;
+    # $tcmd will be put between '' but not escaped
+    
+    my $cmd= "ssh root@$ho->{Ip} '$tcmd'";
+    logm("executing $cmd");
+    alarm($timeout);
+    system $cmd;
+    $? and die $?;
+    alarm(0);
+}
+
 sub opendb_state () {
     $dbh_state= opendb('statedb');
 }
@@ -62,17 +78,19 @@ sub selecthost ($) {
 }
 
 sub poll_loop ($$$&) {
-    my ($interval, $maxwait, $what, $code) = @_;
+    my ($maxwait, $interval, $what, $code) = @_;
     # $code should return undef when all is well
     
     logm("$what: waiting ${maxwait}s...");
-    my $waited= 0;
+    my $start= time;  die $! unless defined $start;
     for (;;) {
         my $bad= $code->();
         last if !defined $bad;
         $waited <= $maxwait or die "$what: wait timed out: $bad.\n";
-        sleep($interval);
         $waited += $interval;
+        my $target= $start+$waited;
+        my $now= time;  die $! unless defined $now;
+        sleep($target - $now) if $now < $target;
     }
     logm("$what: ok.");
 }
@@ -90,7 +108,7 @@ sub power_state ($$) {
         ('SELECT current_power FROM control WHERE asset = ?');
     $sth->bind_param(1, $asset);
     
-    poll_loop(1,30, "power: checking $want", sub {
+    poll_loop(30,1, "power: checking $want", {
         $sth->execute();
         my ($got) = $sth->fetchrow_array();
         return undef if $got eq $want;
@@ -100,6 +118,195 @@ sub power_state ($$) {
 sub logm ($) {
     my ($m) = @_;
     print "LOG $m\n";
+}
+sub file_link_contents ($$$) {
+    my ($fn, $contents) = @_;
+    # $contents may be a coderef in which case we call it with the
+    #  filehandle to allow caller to fill in the file
+    my ($dir, $base, $ext) =
+        $fn =~ m/^( (?: .*/ )? )( [^/]+? )( (?: \.[^./]+ )? )$/x
+        or die "$fn ?";
+    my $real= "$dir$base--osstest$ext";
+    my $linktarg= "$base$ext";
+
+    unlink $real or $!==&ENOENT or die "$real $!";
+    my $flc= new IO::File '>', "$real" or die "$real $!";
+    if (ref $contents eq 'CODE') {
+        $contents->($flc);
+    } else {
+        print $flc $contents or die "$real $!";
+    }
+    close $flc or die "$real $!";
+
+    my $newlink= "$dir$base--newlink$ext";
+
+    if (!lstat "$fn") {
+        $!==&ENOENT or die "$fn $!";
+    } elsif (!-L _) {
+        die "$fn not a symlink";
+        unlink $fn or die "$fn $!";
+    }
+    symlink $linktarg, $newlink or die "$newlink $!";
+    rename $newlink, $fn or die "$newlink $fn $!";
+    logm("wrote $fn");
+}
+
+sub setup_pxeboot ($$) {
+    my ($ho, $bootfile) = @_;
+    my $leaf= $ho->{Ether};
+    $leaf =~ y/A-Z/a-z/;
+    $leaf =~ y/0-9a-f//cd;
+    len($leaf)==12 or die "$leaf";
+    $leaf =~ s/../ $&- /g;
+    $leaf =~ s/\-$//;
+    file_link_contents($c{Tftp}.'/'.$leaf, $bootfile);
+}
+
+sub setup_pxeboot_local ($) {
+    my ($ho) = @_;
+    setup_pxeboot($ho, <<END);
+serial 0 $c{Baud}
+timeout 5
+label local
+	LOCALBOOT 0
+default local
+END
+}
+
+sub await_webspace_fetch_byleaf ($$$$$) {
+    my ($maxwait,$interval,$logtailer, $ho, $url) = @_;
+    my $leaf= $url;
+    $leaf =~ s,.*/,/,;
+    poll_loop($maxwait,$interval, "fetch ...$leaf", {
+        my ($line, $last);
+        $last= '(none)';
+        while (defined($line= $logtailer->getline())) {
+            my ($ip, $got) =
+                m,^([0-9.]+) \S+ \S+ \[[^][]+\] \"GET [^ \t\r/]*/(\S+) ,
+                or next;
+            next unless $ip eq $ho->{Ip};
+            $last= $got;
+            next unless $got eq $leaf;
+            return undef;
+        }
+        return $last;
+    });
+}
+
+sub await_sshd ($$$) {
+    ($maxwait,$interval,$ho) = @_;
+    poll_loop($maxwait,$interval, "await sshd", {
+        my $ncout= `nc -n -v -z -w $interval $ho->{Ip} 22 2>&1`;
+        return undef if !$?;
+        $ncout =~ s/\n/ | /g;
+        return "nc: $? $ncout";
+    });
+}
+
+sub create_webfile ($$$) {
+    my ($ho, $tail, $contents) = @_; # $contents as for file_link_contents
+    my $wf_common= $c{WebspaceCommon}.$ho->{Name}."_".$tail;
+    my $wf_url= $c{WebspaceUrl}.$wf_common;
+    my $wf_file= $c{WebspaceFile}.$wf_common;
+    file_link_contents($wf_file, $contents);
+    return $wf_url;
+}
+
+sub get_filecontents ($;$) {
+    my ($path, $ifnoent) = @_;  # $ifnoent=undef => is error
+    if (!open GFC, '<', $path) {
+        $!==&ENOENT or die "$path $!";
+        return $ifnoent if defined $ifnoent;
+        die "$path does not exist";
+    }
+    local ($/);
+    undef $/;
+    my $data= <GFC>;
+    defined $data or die "$path $!";
+    close GFC or die "$path $!";
+    return $data;
+}
+
+package Osstest::Logtailer;
+
+sub new ($$) {
+    my ($class, $fn) = @_;
+    my $fh= new IO::File, '<', $fn;
+    my $ino= -1;
+    if (!$fh) {
+        $!==&ENOENT or die "$fn $!";
+    } else {
+        seek $fh, 0, SEEK_END or die "$fn $!";
+        stat $fh or die "$fn $!";
+        $ino= (stat _)[1];
+    }
+    my $lt= { Path => $fn, Handle => $fh, Ino => $ino, Buf => '' };
+    bless $lt, $class;
+    return $lt;
+}
+
+sub getline ($) {
+    my ($lt) = @_;
+
+    for (;;) {
+        if ($lt->{Buf} =~ s/^(.*)\n//) {
+            return $1;
+        }
+
+        if ($lt->{Handle}) {
+            seek $lt->{Handle}, 0, SEEK_CUR or die "$lt->{Path} $!";
+
+            my $more;
+            my $got= read $lt->{Handle}, $more, 4096;
+            die "$lt->{Path} $!" unless defined $got;
+            if ($got) {
+                $lt->{Buf} .= $more;
+                next;
+            }
+        }
+
+        if (!stat $lt->{Path}) {
+            $!==&ENOENT or die "$lt->{Path} $!";
+            return undef;
+        }
+        my $nino= (stat _)[1];
+        return undef
+            unless $nino != $lt->{Ino};
+
+        my $nfh= new IO::File, '<', $lt->{Path};
+        if (!$nfh) {
+            $!==&ENOENT or die "$lt->{Path} $!";
+            warn "newly-created $lt->{Path} vanished again";
+            return undef;
+        }
+        stat $nfh or die $!;
+        $nino= (stat _)[1];
+
+        $lt->_close();
+        $lt->{Handle}= $nfh;
+        $lt->{Ino}= $nino;
+    }
+}        
+
+sub _close ($) {
+    my ($lt) = @_;
+    if ($lt->{Handle}) {
+        close $lt->{Handle} or die "$lt->{Path} $!";
+        $lt->{Handle}= undef;
+        $lt->{Ino}= -1;
+    }
+}
+
+sub close ($) {
+    my ($lt) = @_;
+    $lt->_close();
+    $lt->{Buf}= '';
+}
+
+sub DESTROY ($) {
+    my ($lt) = @_;
+    local $!;
+    $lt->_close();
 }
 
 1;
--- config.pl
+++ config.pl
@@ -4,9 +4,27 @@ $c{TestHostDomain}= 'cam.xci-test.com';
 $c{WebspaceFile}= '/export/home/osstest/public_html/';
 $c{WebspaceUrl}= 'http://woking.$c{Domain}/~osstest/';
 $c{WebspaceCommon}= 'osstest/';
+$c{WebspaceLog}= '/var/log/apache2/access.log';
 
 $c{Tftp}= '/tftpboot/pxe';
 
-$c{Host}= 'spider';
+$c{Baud}= 38400;
+$c{PxeDiBase}= 'debian-installer';
+
+$c{Suite}= 'lenny';
+
+$c{Preseed}= <<END;
+d-i mirror/http/hostname string debian.uk.xensource.com
+d-i mirror/http/directory string /debian
+d-i clock-setup/ntp-server string ntp.uk.xensource.com
+END
+
+$r{Host}= 'spider';
+$r{Arch}= 'i386';
+
+$c{AuthorizedKeysFiles}= '';
+$c{AuthorizedKeysAppend}= <<END;
+ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA2m8+FRm8zaCy4+L2ZLsINt3OiRzDu82JE67b4Xyt3O0+IEyflPgw5zgGH69ypOn2GqYTaiBoiYNoAn9bpUksMk71q+co4gsZJ17Acm0256A3NP46ByT6z6/AKTl58vwwNKSCEAzNru53sXTYw2TcCZUN8A4vXY76OeJNJmCmgBDHCNod9fW6+EOn8ZSU1YjFUBV2UmS2ekKmsGNP5ecLAF1bZ8I13KpKUIDIY+UiG0UMwTWDfQY59SNsz6bCxv9NsxSXL29RS2XHFeIQis7t6hJuyZTT4b9YzjEAxvk8kdGzzK6314kwILibm1O1Y8LLyrYsWK1AvnJQFIhcYXF0EQ== iwj@mariner
+END
 
 1;
