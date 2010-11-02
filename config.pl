6140b9ca2bdfa207294961a2e1b1aa73a2108ea3
Ian Jackson <iwj@woking.uk.xensource.com>
2010-11-02 12:26:05 +0000

    adhoc-revtuple-generator uses ${HOME}/repos instead of per-testing-tree repos

--- config.pl
+++ config.pl
@@ -13,6 +13,8 @@ $c{WebspaceUrl}= "http://woking.$c{Domain}/~osstest/";
 $c{WebspaceCommon}= 'osstest/';
 $c{WebspaceLog}= '/var/log/apache2/access.log';
 
+$c{Repos}= "$ENV{HOME}/repos";
+
 $c{GitCache}='teravault-1.cam.xci-test.com:/export/home/xc_osstest/git-cache/';
 $c{GitCacheLocal}= '/home/xc_osstest/git-cache/';
 
