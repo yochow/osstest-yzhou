f633da8e488e252435b7711bc4edd6a010787e80
Ian Jackson <iwj@woking.uk.xensource.com>
2010-11-22 16:43:54 +0000

    Osstest: test script startup creates tmp and cleans it of old files matching t.*

--- Osstest.pm
+++ Osstest.pm
@@ -231,6 +231,9 @@ END
     $stash= "$c{Stash}/$flight/$job";
     ensuredir("$c{Stash}/$flight");
     ensuredir($stash);
+    ensuredir('tmp');
+    system_checked("find tmp -mtime +30 -name t.\\* -print0".
+                   " | xargs -0r rm --");
 }
 
 sub ts_get_host_guest { # pass this @ARGV
